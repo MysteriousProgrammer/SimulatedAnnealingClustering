c93423e1041e3db8b1334ab92bed038fcbd8b07f
Andre <andreo@tuta.io>
2022-04-10 19:13:55 -0600

    Added simulated annealing class with constructor and initalize method

---
+++ sa.py
@@ -0,0 +1,30 @@
+import dataclasses
+import graph
+from typing import List
+
+# Taken from this answer: https://stackoverflow.com/a/2135920
+def split(a, n):
+    k, m = divmod(len(a), n)
+    return (a[i*k+min(i, m):(i+1)*k+min(i+1, m)] for i in range(n))
+
+@dataclasses.dataclass
+class SA:
+    nodes: List[graph.Node]
+    num_clusters: int
+    initial_temp: int
+    final_temp: int
+    temp_update: float
+    iters_per_temp: int
+
+    def initialize_clusters(self):
+        self.clusters = {}
+        divided_nodes = list(split(self.nodes, self.num_clusters))
+        for i in range(self.num_clusters):
+            self.clusters[i] = set(list(divided_nodes[i]))
+        
+
+        print("Initialized {} clusters:".format(self.num_clusters))
+        print(self.clusters)
+
+    def run(self):
+        self.initialize_clusters()
