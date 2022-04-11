6bb5167c23aa18b8a7abb076fd6a430f453d0beb
Andre <andreo@tuta.io>
2022-04-10 19:13:06 -0600

    Fixed neighbors being a dictionary and not a set

--- graph.py
+++ graph.py
@@ -1,8 +1,15 @@
 class Node:
     def __init__(self, name):
         self.name = name
-        self.neighbors = {}
+        self.neighbors = set()
 
     def connect(self, other_node):
         self.neighbors.add(other_node)
-        other_node.neigjbors.add(self)
+        other_node.neighbors.add(self)
+    
+    def __str__(self):
+        return str(self.name)
+
+        
+    def __repr__(self):
+        return str(self.name)
