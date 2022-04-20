e7771ca48540ce95370251fd2ff5844833464aab
souzankor <103328250+souzankor@users.noreply.github.com>
2022-04-20 08:10:45 -0600

    Update sa.py
    Initialized cut def update

--- sa.py
+++ sa.py
@@ -13,7 +13,7 @@ class SA:
     num_clusters: int
     initial_temp: int
     final_temp: int
-    temp_update: float
+    temp_rate_update: float
     iters_per_temp: int
     current_temp: int = 0
 
@@ -22,19 +22,25 @@ class SA:
         divided_nodes = list(split(self.nodes, self.num_clusters))
         for i in range(self.num_clusters):
             self.clusters[i] = set(list(divided_nodes[i]))
-        
-
         print("Initialized {} clusters:".format(self.num_clusters))
         print(self.clusters)
         
     def initialize_cut(self):
-        return
-    
+        for self.node in enumerate(self.initialize_clusters):
+        # situation 1 the current node has neighbers in other clusters
+            if self.neigbers in enumerate(self.initialize_clusters):
+                self.initialize_cut = sum(self.neighbers_num)
+            print("The initialize cut is", self.initialize_cut)
+        # situation 2 the current node has no neighbers in other clusters
+            if self.neighbers not in enumerate(self.initialize_clusters):
+                self.initialize_cut = 0
+            print("The initilize cut is", self.initialize_cut)
+        
     def try_to_accept(self):
         return 
     
     def update_temp(self):
-        return   
+        self.current_temp = self.current_temp * (1-self.temp_rate_update)
     
     def random_move(self):
         return
