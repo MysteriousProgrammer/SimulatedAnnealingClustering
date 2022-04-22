20f9d0dee92579538980ca02dd0d96fc92cf59d0
MysteriousProgrammer <80165580+MysteriousProgrammer@users.noreply.github.com>
2022-04-22 15:04:55 -0600

    Started making update_cut

--- sa.py
+++ sa.py
@@ -26,21 +26,22 @@ class SA:
             self.clusters[i] = list(divided_nodes[i])
         print("Initialized {} clusters:".format(self.num_clusters))
         print(self.clusters)
-        
-    def initialize_cut(self):
-        cut_size = 0
-        node_name_to_cluster_id = dict()
+    
+    def initialize_node_name_to_cluster_id_dict(self):
+        self.node_name_to_cluster_id = dict()
         for cluster_id, nodes in self.clusters.items():
             for node in nodes:
-                node_name_to_cluster_id[node.name] = cluster_id
-
+                self.node_name_to_cluster_id[node.name] = cluster_id
+        
+    def initialize_cut(self):
+        self.cut_size = 0
         for cluster in self.clusters.values():
             for node in cluster:
                 for neighbor in node.neighbors:
-                    if node_name_to_cluster_id[node.name] != node_name_to_cluster_id[neighbor.name]:
-                        cut_size = cut_size + 1
-        cut_size = int(cut_size/2)
-        print("Initial cut size: {}".format(cut_size))
+                    if self.node_name_to_cluster_id[node.name] != self.node_name_to_cluster_id[neighbor.name]:
+                        self.cut_size = self.cut_size + 1
+        self.cut_size = int(self.cut_size/2)
+        print("Initial cut size: {}".format(self.cut_size))
         
     def try_to_accept(self):
         return 
@@ -59,32 +60,47 @@ class SA:
 
     def swap(self, node1, node2):
         print("Swapping nodes {} from cluster {} with {} from cluster {}".format(node1[1], node1[0], node2[1], node2[0]))
-        list_1 = self.clusters[node1[0]]
-        list_2 = self.clusters[node2[0]]
-        list_1.remove(node1[1])
-        list_2.remove(node2[1])
-        list_1.append(node2[1])
-        list_2.append(node1[1])
-        
-    def cal_cost_delt(self):
-        return 0
+        self.clusters[node1[0]].remove(node1[1])
+        self.clusters[node2[0]].remove(node2[1])
+        self.clusters[node1[0]].append(node2[1])
+        self.clusters[node2[0]].append(node1[1])
 
-    def update_cut(self):
+    def update_cut(self, node1, node2):
+        print("Updating cut after moving {} to cluster {} and {} to cluster {}".format(node1[1], node2[0], node2[1], node1[0]))
+
+        # TO-DO: Calculate the increase or decrease in cut size
+        # Notes:
+        # 1. Only consider the neighbors of the moved nodes
+        # 2. Pay very close atention to the cluster id from node1 and node2: they are the OLD cluster
+        node1_object = self.clusters[node2[0]][-1]
+        node2_object = self.clusters[node1[0]][-1]
+        # Go through the neighbors of the node
+        print("Neighbors of node " + node1_object.name)
+        for neighbor in node1_object.neighbors:
+            print(neighbor.name)
+            
+        print("Neighbors of node " + node2_object.name)
+        for neighbor in node2_object.neighbors:
+            print(neighbor.name)
+
         return
     
     def run(self):
         self.current_temp = self.initial_temp
         self.initialize_clusters()
+        self.initialize_node_name_to_cluster_id_dict()
         self.initialize_cut() 
         while self.current_temp > self.final_temp:
             node1, node2 = self.select_two_nodes()
             self.swap(node1, node2)
-            self.cal_cost_delt()
-            if self.cal_cost_delt() > 0:
+            old_cut = self.cut_size
+            self.update_cut(node1, node2)
+            new_cut = self.cut_size
+            delta = old_cut - new_cut
+            if delta > 0:
                 print("Movement is accepted")
             else:
                 self.try_to_accept()
-            self.update_cut()
             self.update_temp()
         
             
